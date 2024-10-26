330b0837bc5733fd1c9cde27163aa0cf64af7410
jy01.lee <jy01.lee@smarterbuild.net>
2024-10-26 22:14:42 +0900

    로그인 백엔드

--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const userSchema = Schema(
   {
@@ -17,6 +20,13 @@ userSchema.methods.toJSON = function () {
   delete obj.createdAt;
   delete obj.updatedAt;
   return obj;
+};
+
+userSchema.methods.generateToken = async function () {
+  const token = await jwt.sign({ _id: this._id }, JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return token;
 };
 
 const User = mongoose.model("User", userSchema);
