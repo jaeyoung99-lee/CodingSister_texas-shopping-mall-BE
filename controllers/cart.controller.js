bb0e12db26cc80f50403484c5c05818abcffcfc3
jy01.lee <jy01.lee@smarterbuild.net>
2024-11-03 07:00:41 +0900

    카트 상품 개 수정하기

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -65,4 +65,31 @@ cartController.deleteCartItem = async (req, res) => {
   }
 };
 
+cartController.editCartItem = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req;
+    const { qty } = req.body;
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "items",
+      populate: {
+        path: "productId",
+        model: "Product",
+      },
+    });
+    if (!cart) {
+      throw new Error("There is no cart for this user");
+    }
+    const index = cart.items.findIndex((item) => item._id.equals(id));
+    if (index === -1) {
+      throw new Error("Can not find item");
+    }
+    cart.items[index].qty = qty;
+    await cart.save();
+    res.status(200).json({ status: 200, data: cart.items });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
 module.exports = cartController;
